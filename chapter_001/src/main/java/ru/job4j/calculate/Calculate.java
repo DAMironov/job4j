330102ac5f9fad01c6506820a188873145eae0e3
DAMironov <xantor3@gmail.com>
2018-11-18 18:41:54 +0300

    1.3. JavaDoc convention [#86336]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,8 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Dmitry Mironov (xantor3@gmail.com)
+*/
+
 public class Calculate{
-	
+	/**
+	* Main
+	*@param args - args.
+	*/
 	public static void main(String args[]){
-		System.out.println("Hello World");
+	System.out.println("Hello World");
+	
 	}
 }

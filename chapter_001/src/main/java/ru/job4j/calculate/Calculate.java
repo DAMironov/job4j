82d3ef56b598d8736ba8ce1bbeb16c850e445456
DAMironov <xantor3@gmail.com>
2018-11-20 20:59:40 +0300

    1.4. JUnit [#86337]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -6,13 +6,20 @@ package ru.job4j.calculate;
 * @author Dmitry Mironov (xantor3@gmail.com)
 */
 
-public class Calculate{
+public class Calculate {
 	/**
 	* Main
 	*@param args - args.
 	*/
-	public static void main(String args[]){
+public static void main(String[] args) {
 	System.out.println("Hello World");
-	
+	}
+	/**
+     * Method echo.
+     * @param name Dmitry Mironov.
+     * @return Echo plus Dmitry Mironov.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
 	}
 }

3c51ba1d03c3f15e378c7a3ef7bf7f0e60157cdc
DAMironov <xantor3@gmail.com>
2018-12-03 22:12:19 +0300

    4.4. Максимум из трех чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -4,4 +4,9 @@ public class Max {
     public int max(int first, int second) {
         return first < 0 ? -first : second;
     }
+
+    public int max(int first, int second, int third) {
+        int temp = this.max(first, second);
+        return max((temp), third);
+    }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -18,4 +18,11 @@ public class MaxTest {
         int result = maxim.max(2, 1);
         assertThat(result, is(1));
     }
+
+    @Test
+    public void whenFirstLessSecondLessThird() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
 }
